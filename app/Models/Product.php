316939eafd065cd4c079e1945dbaffa2aa98944c
fitriaherista <fitriaherista@gmail.com>
2026-01-13 12:13:50 +0700

    menambahkan project fitria pribadi ke branch pribadi

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,18 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
-        'name',
-        'description',
-        'price',
-        'stock',
-        'image',
+        'category_id','name','image','stock','unit','price'
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\RoleMiddleware; //ROLE MIDDLEWARE
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,13 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-
-        // REGISTER MIDDLEWARE ROLE
-        $middleware->alias([
-            'admin' => RoleMiddleware::class . ':admin',
-            'customer' => RoleMiddleware::class . ':customer',
-        ]);
-
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
